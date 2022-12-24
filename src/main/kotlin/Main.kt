d28ac4e9f1d2e9e4d49b3643e37c3ffa443f7d96
Sages0ft <94207840+Sages0ft@users.noreply.github.com>
2022-12-24 15:45:00 +0100

    Commit for test case on reading blue partyhat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,11 @@ import org.openrs2.cache.Cache
 import java.nio.file.Path
 
 fun main(args : Array<String>) {
-    println("Hello, World!")
-
     val path = System.getProperty("user.dir")
-
     println("Working Directory = $path")
+
     val cache = Cache.open(Path.of("./cache/"))
+    val data = cache.read(2,10,1042)
+
+    //cache.write()
 }
