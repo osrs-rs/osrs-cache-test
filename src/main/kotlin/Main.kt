a851bb3a90765258c995fffaf89a3a11d7ba413a
Sages0ft <94207840+Sages0ft@users.noreply.github.com>
2022-12-29 17:00:49 +0100

    Write out the read bytes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import org.openrs2.cache.Cache
 import java.nio.file.Path
 
+fun ByteArray.toHex(): String = joinToString(separator = ", 0x") { eachByte -> "%02x".format(eachByte) }
 fun main(args : Array<String>) {
     val path = System.getProperty("user.dir")
     println("Working Directory = $path")
@@ -8,7 +9,11 @@ fun main(args : Array<String>) {
     val cache = Cache.open(Path.of("./cache/"))
     val data = cache.read(2,10,1042)
 
+    val bytes = ByteArray(data.readableBytes())
+    data.readBytes(bytes)
+
     println("Data len: " + data.capacity())
+    println("Data bytes: 0x" + bytes.toHex())
 
     //cache.write()
 }
